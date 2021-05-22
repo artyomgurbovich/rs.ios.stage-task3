c066eb0c734de1d5415d5615aca0455fe6ff420d
Artyom Gurbovich <artyom.gurbovich@gmail.com>
2021-05-22 21:13:45 +0300

    feat: implement exercise 1

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,53 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    NSArray *months = @[@"January",
+                    @"February",
+                    @"March",
+                    @"April",
+                    @"May",
+                    @"June",
+                    @"July",
+                    @"August",
+                    @"September",
+                    @"October",
+                    @"November",
+                    @"December"];
+    if (monthNumber < 1 || monthNumber > 12) return nil;
+    return [months objectAtIndex:monthNumber-1];
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+    NSDate *dateFromString = [dateFormatter dateFromString:date];
+    NSDateComponents *dateComponents = [calendar components: NSCalendarUnitDay fromDate: dateFromString];
+    return dateComponents.day;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    [calendar setLocale:[NSLocale localeWithLocaleIdentifier: @"ru_RU"]];
+    NSDateComponents *dateComponents = [calendar components: NSCalendarUnitWeekday fromDate: date];
+    if (dateComponents == nil) return nil;
+    
+    return calendar.shortWeekdaySymbols[(dateComponents.weekday - 1) % 7];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *todaysComponents = [calendar components:NSCalendarUnitWeekOfYear fromDate:[NSDate date]];
+    NSUInteger todaysWeek = [todaysComponents weekOfYear];
+    NSDateComponents *otherComponents = [calendar components:NSCalendarUnitWeekOfYear fromDate:date];
+    NSUInteger datesWeek = [otherComponents weekOfYear];
+    return todaysWeek == datesWeek;
 }
 
 @end
